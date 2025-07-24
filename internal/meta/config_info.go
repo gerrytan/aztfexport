73c0f7ec124f2b5ea0eb718f825818fb49836b36
Gerry Tan <gerry.tan@microsoft.com>
2025-07-24 14:35:52 +1000

    Early prototype of data structure

--- internal/meta/config_info.go
+++ internal/meta/config_info.go
@@ -32,8 +32,15 @@ type Dependencies struct {
 	// The key is TFResourceId.
 	ambiguousRefDeps map[string][]Dependency
 
+	// Inferred by checking if the resource has a common attribute with the same value as its parent. For example if
+	// azurerm_virtual_network.res-1.resource_group_name is the same value as its parent azurerm_resource_group.res-0.name,
+	// it will be replaced with the TF address.
+	// The key is TFResourceId of the child resource.
+	commonAttrDeps map[string]CommonAttrDep
+
 	// Dependencies inferred via Azure resource id parent lookup, and will be applied in the "depends_on" block.
-	// Especially, any dependency that is (transitively) present via refDepds will be filtered.
+	// Dependency that's already satisfied via refDeps should not be included here.
+	// TODO: rename this to explicitDeps to avoid confusion with commonAttrDeps?
 	parentChildDeps map[Dependency]bool
 }
 
@@ -41,4 +48,16 @@ type Dependency struct {
 	TFResourceId    string
 	AzureResourceId string
 	TFAddr          tfaddr.TFAddr
+}
+
+type CommonAttrDep struct {
+	// AzureResourceId of the parent resource, used to mark that a dependency relation is established
+	AzureResourceId string
+
+	// TFAddr of the parent resource, for example: azurerm_resource_group.res-0
+	TFAddr tfaddr.TFAddr
+
+	// The attribute name mapping between child -> parent. For example: "resource_group_name" -> "name"
+	// This will be used by hcl_edit.go to replace "resource_group_name" = "my-rg" with "resource_group_name" = azurerm_resource_group.res-0.name
+	AttrMap map[string]string
 }
